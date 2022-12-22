df3defa76472af3b8326cf4ff7bc99ca059f3b5a
Master-Bw3 <bw1jochman@gmail.com>
2022-12-22 10:48:56 -0600

    delete everything

--- README.md
+++
@@ -1,3 +0,0 @@
-# Hex-Studio
-An IDE for hexcasting
-https://master-bw3.github.io/Hex-Studio/
--- scripts/Pattern/Drawn_Patterns.js
+++
@@ -1,9 +0,0 @@
-var drawn_patterns = Array();
-
-export default drawn_patterns;
-export function set_drawn_patterns(values) {
-    drawn_patterns.length = 0;
-    values.forEach((values) => {
-        drawn_patterns.push(values);
-    });
-}
